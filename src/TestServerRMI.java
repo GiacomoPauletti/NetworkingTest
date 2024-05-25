fff3067e8d105e942bd10af39f10861a2f84eb82
Giacomo <giacomo.pauletti@gmail.com>
2024-05-25 18:51:04 +0200

    Bla bla car 3

--- src/TestServerRMI.java
+++ src/TestServerRMI.java
@@ -1,12 +1,28 @@
+import java.io.IOException;
+import java.io.Serial;
+import java.io.Serializable;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketOption;
 import java.rmi.AlreadyBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.rmi.server.RMIClientSocketFactory;
+import java.rmi.server.RMIServerSocketFactory;
 import java.rmi.server.UnicastRemoteObject;
 
-public class TestServerRMI extends UnicastRemoteObject implements ITestServerRMI  {
-    public TestServerRMI() throws RemoteException {
-        super();
+public class TestServerRMI implements ITestServerRMI  {
+    public TestServerRMI() {
+
+        try {
+            UnicastRemoteObject.exportObject(this, 0, new MyRMIClientSocketFactory()
+            , port -> new ServerSocket(port));
+        } catch (RemoteException e) {
+            throw new RuntimeException(e);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
         /*
         try {
             UnicastRemoteObject.exportObject(this, 0);
@@ -23,17 +39,40 @@ public class TestServerRMI extends UnicastRemoteObject implements ITestServerRMI
         } catch (RemoteException e) {
             throw new RuntimeException(e);
         } catch (AlreadyBoundException e) {
-            throw new RuntimeException(e);
+            //throw new RuntimeException(e);
         }
     }
 
     @Override
     public void sayHello(ITestClientRMI client) throws RemoteException {
-        System.out.println("Client said hello");
-        try {
-            client.helloBack();
-        } catch ( RemoteException e ) {
-            e.printStackTrace();
+        while ( true ) {
+            System.out.println("Client said hello");
+            try {
+                client.helloBack();
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+
         }
     }
+
+    public class MyRMIClientSocketFactory implements RMIClientSocketFactory, Serializable {
+        @Override
+        public Socket createSocket(String host, int port) throws IOException {
+            Socket socket = new Socket(host, port);
+            socket.setSoTimeout(15 * 1000);
+            return socket;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            return obj.getClass().equals(this.getClass());
+        }
+
+        @Override
+        public int hashCode() {
+            return super.hashCode();
+        }
+    }
+
 }
